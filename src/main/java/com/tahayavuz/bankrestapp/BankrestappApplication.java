7f539cad93ae4ec4053816d2f2d15b0332e75e7a
Taha Yavuz <45938952+tahayvz@users.noreply.github.com>
2022-07-03 22:04:36 +0300

    first commit

---
+++ src/main/java/com/tahayavuz/bankrestapp/BankrestappApplication.java
@@ -0,0 +1,13 @@
+package com.tahayavuz.bankrestapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankrestappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankrestappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tahayavuz/bankrestapp/BankrestappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tahayavuz.bankrestapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankrestappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
